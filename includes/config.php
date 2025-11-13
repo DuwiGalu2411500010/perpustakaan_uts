029435fd63d84849c69a7dc971b9cd9c7d55e4ce
Galu <2411500010@mahasiswa.atmaluhur.ac.id>
2025-11-13 14:37:33 +0700

    add: config.php

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'db_perpustakaan';
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    die("Koneksi gagal: ". $mysqli->connect_error);
+}
+?>
